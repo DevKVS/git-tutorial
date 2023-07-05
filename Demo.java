89d49cb5dd6ef9948c38f40bb710d946124c4168
= <krsvnstrdnckgm@gmail.com>
2023-07-05 20:05:20 +0200

    change to ssh

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Demo");
-        System.out.println("Version 3 new");
+        System.out.println("Version 3 ssh");
     }
 }
