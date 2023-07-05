6805391115fef316cdf95af4b11e74c49fca56d5
= <krsvnstrdnckgm@gmail.com>
2023-07-05 16:41:50 +0200

    increment version and update gitignore

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Main");
-        System.out.println("Version 1");
+        System.out.println("Version 2");
     }
 }
